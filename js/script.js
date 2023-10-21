961d5a5b8e40e44e284e3f50360a7b15090d353f
Jewel Jean Teves <jeweljeansotto@gmail.com>
2023-10-21 22:11:27 +0800

    atoda

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const downloadBtn = document.querySelector(".btn-box");
+
+
+// google drive's app link
+const fileLink = "https://drive.google.com/file/d/1agGOXGGjMaJ-BaZ6abI5G1eUVBnAHEsY/view?usp=share_link";
+
+const initTimer = () => {
+    // getting data-timer attribute from html
+}
+
+downloadBtn.addEventListener("click", initTimer)
